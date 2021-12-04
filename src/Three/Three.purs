13c588a15e880155eadaf636e90c819d4daeea6f
Tom Smilack <tom@smilack.net>
2021-12-04 17:27:55 -0600

    Created folder for day 3

---
+++ src/Three/Three.purs
@@ -0,0 +1,16 @@
+module AdventOfCode.Twenty21.Three
+  ( main
+  ) where
+
+import Prelude
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console (log, logShow)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile)
+
+main :: Effect Unit
+main = launchAff_ do
+  input <- readTextFile UTF8 "./src/Three/input"
+  liftEffect $ log input
