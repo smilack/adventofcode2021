065a3fb04a697e1e65b668e47bdcd9b9b5038044
Tom Smilack <tom@smilack.net>
2021-12-03 20:41:59 -0600

    D2P2 solution

--- src/Two/Two.purs
+++ src/Two/Two.purs
@@ -32,11 +32,18 @@ main = launchAff_ do
     tokens = readCommandArray input
     commands = parseCommands tokens
     position = unwrap $ foldl (<>) mempty commands
+    position2 = runCommands2 tokens
   liftEffect do
+    log "Part 1:"
     log "Position is:"
     logShow position
     log "Depth × Forward is:"
     logShow (position.depth * position.forward)
+    log "Part 2:"
+    log "Position is:"
+    logShow position2
+    log "Depth × Forward is:"
+    logShow (position2.depth * position2.forward)
 
 readCommandArray :: String -> Array CommandToken
 readCommandArray =
@@ -70,3 +77,16 @@ instance Semigroup Command where
 
 instance Monoid Command where
   mempty = Command { depth: 0, forward: 0 }
+
+type AimCommand = { depth :: Int, forward :: Int, aim :: Int }
+
+runCommands2 :: Array CommandToken -> AimCommand
+runCommands2 =
+  foldl f { depth: 0, forward: 0, aim: 0 }
+  where
+  f position@{ depth, forward, aim } (CommandToken s n)
+    | s == "up" = position { aim = aim - n }
+    | s == "down" = position { aim = aim + n }
+    | s == "forward" =
+        position { depth = depth + aim * n, forward = forward + n }
+    | otherwise = position
