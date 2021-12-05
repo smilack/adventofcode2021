b690096e478c3bb97c2060d13e3c878235a7fa8c
Tom Smilack <tom@smilack.net>
2021-12-05 17:04:52 -0600

    Add a test for D5

--- test/Five/Five.purs
+++ test/Five/Five.purs
@@ -13,4 +13,12 @@ import Test.QuickCheck
 main :: Effect Unit
 main = do
   log "Five"
+  log "parseLine"
+  quickCheck testParseLine
 
+testParseLine :: Int -> Int -> Int -> Int -> Boolean
+testParseLine x1 y1 x2 y2 =
+  ({ x1, y1, x2, y2 } == _) $ parseLine $ toString x1 y1 x2 y2
+  where
+  toString x1 y1 x2 y2 =
+    show x1 <> "," <> show y1 <> " -> " <> show x2 <> "," <> show y2
