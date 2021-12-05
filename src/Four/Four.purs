bd23335ac0a9b9bbcaeceb6801ab320d900c84c6
Tom Smilack <tom@smilack.net>
2021-12-05 15:23:21 -0600

    D4P2 solution

--- src/Four/Four.purs
+++ src/Four/Four.purs
@@ -3,10 +3,12 @@ module AdventOfCode.Twenty21.Four
   ) where
 
 import Prelude
-import Data.Array (filter, concat, any, null, delete)
+import Data.Array (concat, any, delete)
+import Data.Array (null, filter) as A
 import Data.Foldable (sum)
 import Data.Int (fromString)
 import Data.List (List(..), (:), head, tail, fromFoldable, transpose, toUnfoldable, find)
+import Data.List (null, filter) as L
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
@@ -22,6 +24,8 @@ import Node.FS.Aff (readTextFile)
 --           add up the uncalled numbers on the winner, and multiply the sum by
 --           the number called last.
 
+-- Part Two: Same except find the LAST winning board
+
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./src/Four/input"
@@ -31,13 +35,20 @@ main = launchAff_ do
     boards = getBoards $ tail $ lines
     winner = findWinner draws boards
     score = calculateScore winner
+    lastWinner = findLastWinner draws boards
   liftEffect do
+    log "Part 1:"
     log "Draws:"
     logShow draws
     log "Winning board:"
     logShow $ winner.board.rows
     log "Score:"
     logShow $ score
+    log "Part 2:"
+    log "Last winning board:"
+    logShow $ lastWinner.board.rows
+    log "Score:"
+    logShow $ calculateScore lastWinner
 
 getDraws :: Maybe String -> List Int
 getDraws = case _ of
@@ -67,7 +78,7 @@ getBoards = case _ of
   parseRow =
     fromMaybe []
       <<< sequence
-      <<< filter (_ /= Nothing)
+      <<< A.filter (_ /= Nothing)
       <<< map fromString
       <<< split (Pattern " ")
 
@@ -84,7 +95,7 @@ type Winner = { board :: Board, draw :: Int }
 findWinner :: List Int -> List Board -> Winner
 findWinner = go
   where
-  go Nil _ = { board: { rows: [], cols: [] }, draw: 0 }
+  go Nil _ = winnerError
   go (draw : draws) boards =
     let
       boardsWithoutDraw = map (remove draw) boards
@@ -94,12 +105,34 @@ findWinner = go
         Just board -> { board, draw }
         Nothing -> go draws boardsWithoutDraw
 
-  remove draw { rows, cols } =
-    { rows: map (delete draw) rows
-    , cols: map (delete draw) cols
-    }
+findLastWinner :: List Int -> List Board -> Winner
+findLastWinner = go
+  where
+  go Nil _ = winnerError
+  go (draw : draws) boards =
+    let
+      boardsWithoutDraw = map (remove draw) boards
+      winner = find hasWon boardsWithoutDraw
+      nonWinners = L.filter (not <<< hasWon) boardsWithoutDraw
+    in
+      if L.null nonWinners then
+        case winner of
+          Just board -> { board, draw }
+          Nothing -> winnerError
+      else
+        go draws nonWinners
 
-  hasWon board = any null $ board.rows <> board.cols
+winnerError :: Winner
+winnerError = { board: { rows: [], cols: [] }, draw: 0 }
+
+remove :: Int -> Board -> Board
+remove draw { rows, cols } =
+  { rows: map (delete draw) rows
+  , cols: map (delete draw) cols
+  }
+
+hasWon :: Board -> Boolean
+hasWon board = any A.null $ board.rows <> board.cols
 
 calculateScore :: Winner -> Int
 calculateScore { board, draw } = uncalledSum * draw
