10eb1a746cb1e566ac8a83c4d8800e59b09bbe84
Tom Smilack <tom@smilack.net>
2021-12-04 21:19:01 -0600

    Create Four and download input file

---
+++ src/Four/Four.purs
@@ -0,0 +1,18 @@
+module AdventOfCode.Twenty21.Four
+  ( main
+  ) where
+
+import Prelude
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console (log, logShow)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile)
+
+main :: Effect Unit
+main = launchAff_ do
+  input <- readTextFile UTF8 "./src/Four/input"
+  liftEffect $ log input
