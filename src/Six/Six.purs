2307bf89b0827ad663a8b83d080b82e8a8c92dc7
Tom Smilack <tom@smilack.net>
2021-12-07 14:42:31 -0600

    Make skeletons for day 6

---
+++ src/Six/Six.purs
@@ -0,0 +1,16 @@
+module AdventOfCode.Twenty21.Six where
+
+import Prelude
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console (log, logShow)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile)
+
+main :: Effect Unit
+main = launchAff_ do
+  input <- readTextFile UTF8 "./src/Six/input"
+  liftEffect $ log input
