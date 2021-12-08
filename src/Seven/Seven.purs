5cff2c14d8694535aaf8b074beb291e2d75faf63
Tom Smilack <tom@smilack.net>
2021-12-07 21:49:44 -0600

    D7P1 solution

--- src/Seven/Seven.purs
+++ src/Seven/Seven.purs
@@ -1,6 +1,12 @@
 module AdventOfCode.Twenty21.Seven where
 
 import Prelude
+import Data.Array (catMaybes)
+import Data.Array.NonEmpty (NonEmptyArray, nub, sortWith, head, singleton, fromArray)
+import Data.Foldable (sum)
+import Data.Int (fromString)
+import Data.Maybe (fromMaybe)
+import Data.Ord (abs)
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
@@ -10,7 +16,33 @@ import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
 
+-- Part one: Given a list of horizontal positions, find the position that, if
+--           all were shifted to that position, there would be the least total
+--           change. Return the sum of how far each moved.
+
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./src/Seven/input"
-  liftEffect $ log input
+  liftEffect do
+    log "Part 1:"
+    log "Best fuel use:"
+    logShow $ solvePart1 input
+
+parseInput :: String -> NonEmptyArray Int
+parseInput =
+  fromMaybe (singleton 0)
+    <<< fromArray
+    <<< catMaybes
+    <<< map fromString
+    <<< split (Pattern ",")
+
+cumulativeDistanceTo :: Int -> NonEmptyArray Int -> Int
+cumulativeDistanceTo p = sum <<< map (abs <<< (_ - p))
+
+bestPosition :: NonEmptyArray Int -> Int
+bestPosition xs = head $ sortWith (\p -> cumulativeDistanceTo p xs) $ nub xs
+
+solvePart1 :: String -> Int
+solvePart1 i = cumulativeDistanceTo (bestPosition input) input
+  where
+  input = parseInput i
--- test/Seven/Seven.purs
+++ test/Seven/Seven.purs
@@ -4,6 +4,8 @@ module Test.AdventOfCode.Twenty21.Seven
 
 import Prelude
 import AdventOfCode.Twenty21.Seven
+import Data.Array.NonEmpty (NonEmptyArray, singleton, appendArray)
+import Data.List (List(..), (:))
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
@@ -18,6 +20,25 @@ import Test.Spec.Runner (runSpec)
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Day Seven" do
-    pending "parse input"
-    pending "other stuff"
+    describe "Input" do
+      it "parses csv" do
+        input `shouldEqual` inputCheck
+      it "calculates fuel use" do
+        cumulativeDistanceTo 2 input `shouldEqual` 37
+        cumulativeDistanceTo 1 input `shouldEqual` 41
+        cumulativeDistanceTo 3 input `shouldEqual` 39
+        cumulativeDistanceTo 10 input `shouldEqual` 71
+      it "finds minimum fuel position" do
+        bestPosition input `shouldEqual` 2
+      it "finds fuel use of best position" do
+        solvePart1 strInput `shouldEqual` 37
+
+strInput :: String
+strInput = "16,1,2,0,4,2,7,1,2,14"
+
+input :: NonEmptyArray Int
+input = parseInput strInput
+
+inputCheck :: NonEmptyArray Int
+inputCheck = appendArray (singleton 16) [ 1, 2, 0, 4, 2, 7, 1, 2, 14 ]
 
