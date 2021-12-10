1af192b779a6f335b98dea1418c7153ef1035652
Tom Smilack <tom@smilack.net>
2021-12-10 00:36:10 -0600

    Parse D10 input

--- src/Ten/Ten.purs
+++ src/Ten/Ten.purs
@@ -1,8 +1,11 @@
 module AdventOfCode.Twenty21.Ten where
 
 import Prelude
-import Data.String (split)
-import Data.String.Pattern (Pattern(..))
+import Data.Eq.Generic (genericEq)
+import Data.Generic.Rep (class Generic)
+import Data.List (List(..), (:), fromFoldable, catMaybes)
+import Data.Maybe (Maybe(..))
+import Data.String.Utils (lines, toCharArray)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -14,3 +17,52 @@ main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./src/Ten/input"
   liftEffect $ log input
+
+parseInput :: String -> List (List Character)
+parseInput =
+  map (catMaybes <<< map readCharacter)
+    <<< map (fromFoldable <<< toCharArray)
+    <<< fromFoldable
+    <<< lines
+
+data OpenClose
+  = Open
+  | Close
+
+derive instance Generic OpenClose _
+
+instance Eq OpenClose where
+  eq = genericEq
+
+data Character
+  = Paren OpenClose
+  | Bracket OpenClose
+  | Brace OpenClose
+  | Angle OpenClose
+
+derive instance Generic Character _
+
+instance Eq Character where
+  eq = genericEq
+
+instance Show Character where
+  show (Paren Open) = "("
+  show (Paren Close) = ")"
+  show (Bracket Open) = "["
+  show (Bracket Close) = "]"
+  show (Brace Open) = "{"
+  show (Brace Close) = "}"
+  show (Angle Open) = "<"
+  show (Angle Close) = ">"
+
+readCharacter :: String -> Maybe Character
+readCharacter = case _ of
+  "(" -> Just $ Paren Open
+  ")" -> Just $ Paren Close
+  "[" -> Just $ Bracket Open
+  "]" -> Just $ Bracket Close
+  "{" -> Just $ Brace Open
+  "}" -> Just $ Brace Close
+  "<" -> Just $ Angle Open
+  ">" -> Just $ Angle Close
+  _ -> Nothing
--- test/Ten/Ten.purs
+++ test/Ten/Ten.purs
@@ -4,8 +4,7 @@ module Test.AdventOfCode.Twenty21.Ten
 
 import Prelude
 import AdventOfCode.Twenty21.Ten
-import Data.String (split)
-import Data.String.Pattern (Pattern(..))
+import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.QuickCheck ((===), Result)
@@ -18,6 +17,32 @@ import Test.Spec.Runner (runSpec)
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Day Ten" do
-    pending "parse input"
-    pending "other stuff"
+    it "parses input" do
+      parseInput testInput `shouldEqual` parsedTestInput
 
+testInput :: String
+testInput =
+  """[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
+
+parsedTestInput :: List (List Character)
+parsedTestInput =
+  (Bracket Open : Paren Open : Brace Open : Paren Open : Angle Open : Paren Open : Paren Open : Paren Close : Paren Close : Bracket Open : Bracket Close : Angle Close : Bracket Open : Bracket Open : Brace Open : Bracket Open : Bracket Close : Brace Open : Angle Open : Paren Open : Paren Close : Angle Open : Angle Close : Angle Close : Nil)
+    : (Bracket Open : Paren Open : Paren Open : Paren Close : Bracket Open : Angle Open : Angle Close : Bracket Close : Paren Close : Bracket Close : Paren Open : Brace Open : Bracket Open : Angle Open : Brace Open : Angle Open : Angle Open : Bracket Open : Bracket Close : Angle Close : Angle Close : Paren Open : Nil)
+    : (Brace Open : Paren Open : Bracket Open : Paren Open : Angle Open : Brace Open : Brace Close : Bracket Open : Angle Open : Angle Close : Bracket Open : Bracket Close : Brace Close : Angle Close : Brace Open : Bracket Open : Bracket Close : Brace Open : Bracket Open : Paren Open : Angle Open : Paren Open : Paren Close : Angle Close : Nil)
+    : (Paren Open : Paren Open : Paren Open : Paren Open : Brace Open : Angle Open : Angle Close : Brace Close : Angle Open : Brace Open : Angle Open : Brace Open : Angle Open : Angle Close : Brace Close : Brace Open : Bracket Open : Bracket Close : Brace Open : Bracket Open : Bracket Close : Brace Open : Brace Close : Nil)
+    : (Bracket Open : Bracket Open : Angle Open : Bracket Open : Paren Open : Bracket Open : Bracket Close : Paren Close : Paren Close : Angle Open : Paren Open : Bracket Open : Bracket Open : Brace Open : Brace Close : Bracket Open : Bracket Open : Paren Open : Paren Close : Bracket Close : Bracket Close : Bracket Close : Nil)
+    : (Bracket Open : Brace Open : Bracket Open : Brace Open : Paren Open : Brace Open : Brace Close : Bracket Close : Brace Open : Brace Close : Brace Close : Paren Open : Bracket Open : Brace Open : Bracket Open : Brace Open : Brace Open : Brace Open : Brace Close : Brace Close : Paren Open : Bracket Open : Bracket Close : Nil)
+    : (Brace Open : Angle Open : Bracket Open : Bracket Open : Bracket Close : Bracket Close : Angle Close : Brace Close : Angle Open : Brace Open : Bracket Open : Brace Open : Bracket Open : Brace Open : Bracket Open : Bracket Close : Brace Open : Paren Open : Paren Close : Bracket Open : Bracket Open : Bracket Open : Bracket Close : Nil)
+    : (Bracket Open : Angle Open : Paren Open : Angle Open : Paren Open : Angle Open : Paren Open : Angle Open : Brace Open : Brace Close : Paren Close : Paren Close : Angle Close : Angle Open : Paren Open : Bracket Open : Bracket Close : Paren Open : Bracket Open : Bracket Close : Paren Open : Paren Close : Nil)
+    : (Angle Open : Brace Open : Paren Open : Bracket Open : Paren Open : Bracket Open : Bracket Open : Paren Open : Angle Open : Angle Close : Paren Open : Paren Close : Paren Close : Brace Open : Brace Close : Bracket Close : Angle Close : Paren Open : Angle Open : Angle Open : Brace Open : Brace Open : Nil)
+    : (Angle Open : Brace Open : Paren Open : Bracket Open : Brace Open : Brace Open : Brace Close : Brace Close : Bracket Open : Angle Open : Bracket Open : Bracket Open : Bracket Open : Angle Open : Angle Close : Brace Open : Brace Close : Bracket Close : Bracket Close : Bracket Close : Angle Close : Bracket Open : Bracket Close : Bracket Close : Nil)
+    : Nil
