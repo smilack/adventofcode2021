232b03880818d2be638a1ee006a9a8150cc9ac18
Tom Smilack <tom@smilack.net>
2021-12-13 20:44:48 -0600

    Grid: FunctorWithIndex, Foldable, FoldableWithIndex, comment cleanup, tests for functor laws, folding, mapping

--- src/Eleven/Grid.purs
+++ src/Eleven/Grid.purs
@@ -1,20 +1,23 @@
 module AdventOfCode.Twenty21.Eleven.Grid
   ( Grid
-  -- , Adjacent
-  -- , empty
-  -- , alone
-  -- , singleton
-  -- , fromFoldables
-  -- , fromMaybe
+  , Point
+  , fromSize
+  , fromSizeWithDefault
   ) where
 
 import Prelude
 import Data.Array (fromFoldable) as Array
 import Data.Array.NonEmpty (NonEmptyArray, (!!), replicate, range, updateAt)
-import Data.Foldable (class Foldable, foldl, foldr, foldMap, foldrDefault, foldlDefault)
+import Data.Foldable (class Foldable, foldMap, foldrDefault, foldlDefault, fold)
+import Data.FoldableWithIndex (class FoldableWithIndex, foldMapWithIndex, foldrWithIndexDefault, foldlWithIndexDefault)
+import Data.FunctorWithIndex (class FunctorWithIndex, mapWithIndex)
 import Data.Maybe (Maybe(..))
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (chooseInt)
+
+-----------
+-- Types --
+-----------
 
 type Point = { x :: Int, y :: Int }
 
@@ -22,10 +25,9 @@ type Size = { width :: Int, height :: Int }
 
 data Grid a = Grid Size (NonEmptyArray (NonEmptyArray a))
 
--- instance Foldable Grid where
---   foldMap f g = 
---   foldl = foldlDefault
---   foldr = foldrDefault
+------------------
+-- Type Classes --
+------------------
 
 instance Eq a => Eq (Grid a) where
   eq (Grid s1 r1) (Grid s2 r2) = s1 == s2 && r1 == r2
@@ -45,6 +47,27 @@ instance Arbitrary a => Arbitrary (Grid a) where
 instance Functor Grid where
   map f (Grid size rows) = Grid size (map (map f) rows)
 
+instance FunctorWithIndex Point Grid where
+  mapWithIndex :: forall a b. (Point -> a -> b) -> Grid a -> Grid b
+  mapWithIndex f (Grid size rows) = Grid size rows'
+    where
+    rows' = mapWithIndex mapInner rows
+    mapInner y row = mapWithIndex (\x a -> f { x, y } a) row
+
+instance Foldable Grid where
+  foldMap f (Grid _ rows) = fold (map (foldMap f) rows)
+  foldl f b g = foldlDefault f b g
+  foldr f b g = foldrDefault f b g
+
+instance FoldableWithIndex Point Grid where
+  foldMapWithIndex f grid = foldMap identity $ mapWithIndex f grid
+  foldlWithIndex f b g = foldlWithIndexDefault f b g
+  foldrWithIndex f b g = foldrWithIndexDefault f b g
+
+------------------
+-- Constructors --
+------------------
+
 fromSizeWithDefault :: forall a. Size -> a -> Grid a
 fromSizeWithDefault size@{ width, height } a = Grid size rows
   where
@@ -57,70 +80,19 @@ fromSizeWithDefault size@{ width, height } a = Grid size rows
 fromSize :: forall m. Monoid m => Size -> Grid m
 fromSize size = fromSizeWithDefault size mempty
 
+----------------
+-- Operations --
+----------------
+
 get :: forall a. Point -> Grid a -> Maybe a
 get { x, y } (Grid _ rows) = join $ map (_ !! x) $ (rows !! y)
 
 set :: forall a. Point -> a -> Grid a -> Maybe (Grid a)
 set { x, y } a (Grid size rows) = grid'
   where
-  -- grid' = map (\rs' -> Grid size rows') rows'
-
-  -- rows' :: Maybe (Maybe (NonEmptyArray (NonEmptyArray a)))
-  -- rows' = map (\r' -> updateAt y r' rows) row'
-
-  -- row' :: Maybe (NonEmptyArray a)
-  -- row' = join $ map (updateAt x a) $ (rows !! y)
-
   grid' :: Maybe (Grid a)
   grid' = do
     row <- rows !! y
     row' <- updateAt x a row
     rows' <- updateAt y row' rows
     pure $ Grid size rows'
-
------------------ old stuff -----------------
-
--- data Grid a
---   = Edge
---   | Cell a (Adjacent a)
-
--- type Adjacent a =
---   { north :: Grid a
---   , northEast :: Grid a
---   , east :: Grid a
---   , southEast :: Grid a
---   , south :: Grid a
---   , southWest :: Grid a
---   , west :: Grid a
---   , northWest :: Grid a
---   }
-
--- empty :: forall a. Grid a
--- empty = Edge
-
--- alone :: forall a. Adjacent a
--- alone =
---   { north: Edge
---   , northEast: Edge
---   , east: Edge
---   , southEast: Edge
---   , south: Edge
---   , southWest: Edge
---   , west: Edge
---   , northWest: Edge
---   }
-
--- singleton :: forall a. a -> Grid a
--- singleton a = Cell a alone
-
--- fromFoldables
---   :: forall a f
---    . Foldable f
---   => f (f a)
---   -> Maybe (Grid a)
--- fromFoldables _ = Nothing
-
--- fromMaybe :: forall a. Maybe (Grid a) -> Grid a
--- fromMaybe = case _ of
---   Just grid -> grid
---   Nothing -> empty
--- test/Eleven/Grid.purs
+++ test/Eleven/Grid.purs
@@ -4,25 +4,39 @@ module Test.AdventOfCode.Twenty21.Eleven.Grid
 
 import Prelude
 import AdventOfCode.Twenty21.Eleven.Grid
+import Data.Foldable (foldl, foldr, foldMap)
+import Data.FoldableWithIndex (foldMapWithIndex)
+import Data.FunctorWithIndex (mapWithIndex)
+import Data.Generic.Rep (class Generic)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
+import Data.Monoid.Additive (Additive(..))
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.QuickCheck ((===), Result)
-import Test.QuickCheck.Arbitrary (class Arbitrary)
+import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary, class Coarbitrary, genericCoarbitrary, arbitraryRecord)
+import Test.QuickCheck.Gen (perturbGen)
 import Test.Spec (Spec, pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
+import Prim.RowList (Cons, Nil)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Grid" do
-    describe "obeys functor laws" do
-      it "identity" $ quickCheck idLaw
-      it "composition" $ quickCheck compLaw
+    functorLaws
+    functorWithIndexLaws
+    foldingAndMapping
+
+functorLaws :: Spec Unit
+functorLaws = do
+  describe "Functor laws" do
+    it "Identity" $ quickCheck idLaw
+    it "Composition" $ quickCheck compLaw
 
   where
   idLaw :: Grid Int -> Result
@@ -35,3 +49,58 @@ main = launchAff_ $ runSpec [ consoleReporter ] do
     -> Result
   compLaw f g grid =
     map f (map g grid) === map (f <<< g) grid
+
+functorWithIndexLaws :: Spec Unit
+functorWithIndexLaws = do
+  describe "FunctorWithIndex laws" do
+    it "Identity" $ quickCheck idWILaw
+    it "Composition" $ quickCheck compWILaw
+
+  where
+  idWILaw :: Grid Int -> Result
+  idWILaw grid =
+    mapWithIndex (const identity) grid === identity grid
+
+  compWILaw
+    :: (Point' -> Number -> String)
+    -> (Point' -> Int -> Number)
+    -> Grid Int
+    -> Result
+  compWILaw f g grid =
+    mapWithIndex (f <<< Point') (mapWithIndex (g <<< Point') grid)
+      === mapWithIndex (\i -> f (Point' i) <<< g (Point' i)) grid
+
+newtype Point' = Point' Point
+
+instance Coarbitrary Point' where
+  coarbitrary (Point' { x, y }) = perturbGen <<< toNumber $ x * y
+
+foldingAndMapping :: Spec Unit
+foldingAndMapping = do
+  describe "Folding and Mapping" do
+    it "Map add5" do
+      map (_ <> Additive 5) grid1 `shouldEqual` grid2
+    it "Fold Additive" do
+      foldMap identity grid1 `shouldEqual` (Additive 0)
+      foldMap identity grid2 `shouldEqual` (Additive 20)
+    it "FoldMapWithIndex Point" do
+      foldMapWithIndex valueToCoords grid1 `shouldEqual` "(0, 0)(1, 0)(0, 1)(1, 1)"
+    it "foldl vs foldr" do
+      foldl (\acc val -> acc <> show val) "" gridAsc `shouldEqual` "0123"
+      foldr (\val acc -> show val <> acc) "" gridAsc `shouldEqual` "0123"
+
+  where
+  grid1 :: Grid (Additive Int)
+  grid1 = fromSize { width: 2, height: 2 }
+
+  grid2 :: Grid (Additive Int)
+  grid2 = fromSizeWithDefault { width: 2, height: 2 } $ Additive 5
+
+  valueToCoords :: forall a. Point -> a -> String
+  valueToCoords { x, y } _ =
+    "(" <> show x <> ", " <> show y <> ")"
+
+  gridAsc :: Grid Int
+  gridAsc =
+    mapWithIndex (\{ x, y } _ -> x + 2 * y)
+      $ fromSizeWithDefault { width: 2, height: 2 } 0
